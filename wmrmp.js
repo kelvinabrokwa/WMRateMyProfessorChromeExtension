c93e983c5b9c8780708ac2a6f18979165097a189
kelvinabrokwa <kelvinabrokwa@gmail.com>
2015-03-14 01:48:42 -0400

    cleaner javascript

--- wmrmp.js
+++ wmrmp.js
@@ -1,90 +1,90 @@
-//Written by Nathan Owen for the use of William and Mary students.
-//References to the acronym RMP mean "Rate My Professor"
+/*global $:false */
+/*global chrome:false*/
 
+/**
+ *
+ * Written by Nathan Owen for the use of William and Mary students.
+ * References to the acronym RMP mean "Rate My Professor"
+ *
+ * Manual Exceptions - some professors have their names listed differently on RMP than they do on the open course
+ *                    list. This exceptions dictionary is used manually include references to such pages.
+ *                    For instance, "Noonan, Deborah" is Debbie Noonan on RMP and "Peers, Pieter" is Peter Peers.
+ */
 
+var exceptions = {};
+exceptions['Noonan, Deborah'] = 'http://www.ratemyprofessors.com/ShowRatings.jsp?tid=23060';
+exceptions['Peers, Pieter'] = 'http://www.ratemyprofessors.com/AddRating.jsp?tid=1572567';
 
-//Manual Exceptions - some professors have their names listed differently on RMP than they do on the open course
-//                    list. This exceptions dictionary is used manually include references to such pages.
-//                    For instance, "Noonan, Deborah" is Debbie Noonan on RMP and "Peers, Pieter" is Peter Peers.
-var exceptions = {}
-exceptions["Noonan, Deborah"] = "http://www.ratemyprofessors.com/ShowRatings.jsp?tid=23060"
-exceptions["Peers, Pieter"] = "http://www.ratemyprofessors.com/AddRating.jsp?tid=1572567"
-
-
-//Main
+// Main
 var numClasses = $('table tr:last').index() + 1;
 
-//If more than 1000 classes, it can take a while to load, so we give the user the option to load with a button.
-if (numClasses <= 1000){
-	appendRMPColumn(exceptions);
-}
-else{
-
-	var btn = document.createElement("BUTTON");
-	var show = document.createTextNode("Show Rate My Professor Ratings");
-	var warning = document.createTextNode("There are more than 1000 classes on this page; the load might take a while. Use at your own risk :)")
-	btn.appendChild(show);
-	btn.className = "srchButtons"
-	btn.onclick = function() { 
-		btn.innerHTML = "Refresh Rate My Professor Ratings";
-		appendRMPColumn(exceptions); 
-	};
-	document.getElementById("h1").appendChild(btn);
-	document.getElementById("h1").appendChild(warning);
-
-}
-
 /*
-Insert a column with the ratings and corresponding professor pages for each professor from Ratemyprofessor.com
+If more than 1000 classes, it can take a while to load, so we give the user the option to load with a button.
 */
-function appendRMPColumn(exceptions){
+if (numClasses <= 1000){
+    appendRMPColumn(exceptions);
+} else {
 
-	//Insert a column header
-	$("thead").find('tr').each(function(){
-		$(this).find('th').eq(4).after('<th>Rate My Professor Score(s)</th>');
-	});
+    var btn = document.createElement('BUTTON');
+    var show = document.createTextNode('Show Rate My Professor Ratings');
+    var warning = document.createTextNode('There are more than 1000 classes on this page; the load might take a while. Use at your own risk :)');
+    btn.appendChild(show);
+    btn.className = 'srchButtons';
+    btn.onclick = function() { 
+        btn.innerHTML = 'Refresh Rate My Professor Ratings';
+        appendRMPColumn(exceptions); 
+    };
+    document.getElementById('h1').appendChild(btn);
+    document.getElementById('h1').appendChild(warning);
+}
 
-	var processedProfessors = [];//for keeping track of duplicate professors
+//Insert a column with the ratings and corresponding professor pages for each professor from Ratemyprofessor.com
 
-	//Insert the column by adding an individual cell to each row.
-	$("tbody").find('tr').each(function(){
+function appendRMPColumn(exceptions) {
 
-		//Get and transform the professor's name form "last, first" form to "last(+middle)+first" form so it may be appended to the search URL
-		var professorName = $(this).find('td').eq(4).html();
-		var URLprofessorName = professorName.replace(/ /g,"+").replace(/,/g,"");
+    //Insert a column header
+    $('thead').find('tr').each(function() {
+        $(this).find('th').eq(4).after('<th>Rate My Professor Score(s)</th>');
+    });
 
+    var processedProfessors = []; //for keeping track of duplicate professors
 
-		if ($.inArray(professorName, processedProfessors) == -1){ //the professor has not been processed yet
+    //Insert the column by adding an individual cell to each row.
+    $('tbody').find('tr').each(function() {
 
-			//make note that the professor is now being processed
-			processedProfessors[processedProfessors.length] = professorName;
+        //Get and transform the professor's name form "last, first" form to "last(+middle)+first" form so it may be appended to the search URL
+        var professorName = $(this).find('td').eq(4).html();
+        var URLprofessorName = professorName.replace(/ /g,'+').replace(/,/g,'');
 
+        if (processedProfessors.indexOf(professorName) === -1) { //the professor has not been processed yet
 
-			//If we already have record of this professor, just use that instead.
-			if (typeof exceptions[professorName] != 'undefined'){
-				findRatings(exceptions[professorName], professorName);
-			}
-			else{
+            //make note that the professor is now being processed
+            processedProfessors.push(professorName);
 
-				var courseId = $(this).find('td').eq(1).html();
-				if (courseId.indexOf("LAW") == -1){//The Law school is listed separately in RMP
+            //If we already have record of this professor, just use that instead.
+            if (exceptions[professorName]) {
+                
+                findRatings(exceptions[professorName], professorName);
+            
+            } else {
 
-					//Create Rate My Professor search URL using the professor name in the correct form.
-					var rmpsearch = "http://www.ratemyprofessors.com/search.jsp?queryBy=teacherName&schoolName=college+of+william+and+mary&queryoption=HEADER&query=PROFESSORNAME&facetSearch=true";
-					rmpsearch = rmpsearch.replace("PROFESSORNAME", URLprofessorName);
-				}
-				else{
-					//Create Rate My Professor search URL using the professor name in the correct form.
-					var rmpsearch = "http://www.ratemyprofessors.com/search.jsp?queryBy=teacherName&schoolName=william+&+mary+school+of+law&queryoption=HEADER&query=PROFESSORNAME&facetSearch=true";
-					rmpsearch = rmpsearch.replace("PROFESSORNAME", URLprofessorName);
-				}
+                var courseId = $(this).find('td').eq(1).html(),
+                    rmpsearch;
+                if (courseId.indexOf('LAW') == -1) { //The Law school is listed separately in RMP
 
-				getProfessorExtension(rmpsearch, professorName);
-			}
+                    //Create Rate My Professor search URL using the professor name in the correct form.
+                    rmpsearch = 'http://www.ratemyprofessors.com/search.jsp?queryBy=teacherName&schoolName=college+of+william+and+mary&queryoption=HEADER&query=PROFESSORNAME&facetSearch=true';
+                    rmpsearch = rmpsearch.replace('PROFESSORNAME', URLprofessorName);
+                } else {
+                    //Create Rate My Professor search URL using the professor name in the correct form.
+                    rmpsearch = 'http://www.ratemyprofessors.com/search.jsp?queryBy=teacherName&schoolName=william+&+mary+school+of+law&queryoption=HEADER&query=PROFESSORNAME&facetSearch=true';
+                    rmpsearch = rmpsearch.replace('PROFESSORNAME', URLprofessorName);
+                }
 
-		}		
-	});
-
+                getProfessorExtension(rmpsearch, professorName);
+            }
+        }     
+    });
 }
 
 /*
@@ -92,38 +92,35 @@ Given an RMP search page, find the numerical extension that corresponds with the
 */
 function getProfessorExtension(searchPageURL, professorName){
 
-	var xmlRequestInfo = {
-	    method: 'GET',
-	    action: 'xhttp',
-	    url: searchPageURL,
-	    professorName: professorName
-	}
+    var xmlRequestInfo = {
+        method: 'GET',
+        action: 'xhttp',
+        url: searchPageURL,
+        professorName: professorName
+    };
 
-	chrome.runtime.sendMessage(xmlRequestInfo, function(data) {
-		
+    chrome.runtime.sendMessage(xmlRequestInfo, function(data) {
+        var responseXML, professorName, ratings;
+        try {
 
-		try{
+            responseXML = data.responseXML;
+            professorName = data.professorName;
 
-			responseXML = data.responseXML;
-			professorName = data.professorName;
+            //Find the numerical extension that leads to the specific professor's RMP page.
+            var professorURLExtension = $(responseXML).find('.listing:first').find('a:first').attr('href');
 
-			//Find the numerical extension that leads to the specific professor's RMP page.
-		    var professorURLExtension = $(responseXML).find(".listing:first").find("a:first").attr('href');
-
-		    //Check to make sure a result was found
-		    if (typeof professorURLExtension === 'undefined'){
-		    	changeRMPCells("?","?", professorName);//update RMP cells with empty information
-		    }
-		    else{
-		    	var professorPageURL = "http://www.ratemyprofessors.com" + professorURLExtension;
-		    	var ratings = findRatings(professorPageURL, professorName);
-		    }
-		}
-
-		catch(err){
-			changeRMPCells("?", "?", professorName);//update RMP cells with empty information
-		}	    
-	});
+            //Check to make sure a result was found
+            if (typeof professorURLExtension === 'undefined'){
+                changeRMPCells('?','?', professorName);//update RMP cells with empty information
+            } else {
+                var professorPageURL = 'http://www.ratemyprofessors.com' + professorURLExtension;
+                ratings = findRatings(professorPageURL, professorName);
+            }
+        }
+        catch(err) {
+            changeRMPCells('?', '?', professorName);//update RMP cells with empty information
+        }       
+    });
 }
 
 /*
@@ -132,50 +129,49 @@ Makes a JSON object containing an overall rating, helpfulness rating, clarity ra
 Then makes a pass to change RMP cells to update each individual cell of the RMP column with this info.
 */
 function findRatings(professorPageURL, professorName){
-	var xmlRequestInfo = {
-	    method: 'GET',
-	    action: 'xhttp',
-	    url: professorPageURL,
-	    professorName: professorName
-	}
+    var xmlRequestInfo = {
+        method: 'GET',
+        action: 'xhttp',
+        url: professorPageURL,
+        professorName: professorName
+    };
 
-	chrome.runtime.sendMessage(xmlRequestInfo, function(data) {
+    chrome.runtime.sendMessage(xmlRequestInfo, function(data) {
 
-		var rating = {
-			overall: -1,
-			helpfulness: -1,
-			clarity: -1,
-			easiness: -1
-		}
+        var rating = {
+            overall: -1,
+            helpfulness: -1,
+            clarity: -1,
+            easiness: -1
+        };
+        var professorName, professorPageURL, responseXML;
+        try {
 
-		try{
+            professorName = data.professorName;
+            professorPageURL = data.url;
+            responseXML = data.responseXML;
 
-			var professorName = data.professorName;
-			var professorPageURL = data.url;
-			var responseXML = data.responseXML;
+            //Find the numerical extension that leads to the specific professor's RMP page.
+            rating.overall = $(responseXML).find('.grade').html();
+            rating.helpfulness = $(responseXML).find('.rating:eq(0)').html();
+            rating.clarity = $(responseXML).find('.rating:eq(1)').html();
+            rating.easiness = $(responseXML).find('.rating:eq(2)').html();
 
-			//Find the numerical extension that leads to the specific professor's RMP page.
-		    rating.overall = $(responseXML).find(".grade").html();
-		    rating.helpfulness = $(responseXML).find(".rating:eq(0)").html();
-		    rating.clarity = $(responseXML).find(".rating:eq(1)").html();
-		    rating.easiness = $(responseXML).find(".rating:eq(2)").html();
+            //document.write(responseXML);
 
-		    //document.write(responseXML);
+            //Check to make sure a result was found
+            if (parseInt(rating.overall) > 5 || parseInt(rating.overall) <= 0 || isNaN(rating.overall)){
+                rating = '?';
+            }
 
-		    //Check to make sure a result was found
-		    if (parseInt(rating.overall) > 5 || parseInt(rating.overall) <= 0 || isNaN(rating.overall)){
-		    	rating = "?";
-		    }
+        }
+        catch(err) {
+            rating = '?';
+        }
 
-		}catch(err){
-			rating = "?";
-		}
-
-		//Update the new RMP column cells with the new information.
-	    changeRMPCells(professorPageURL, rating, professorName);
-	});
-
-
+        //Update the new RMP column cells with the new information.
+        changeRMPCells(professorPageURL, rating, professorName);
+    });
 }
 
 /*
@@ -183,36 +179,33 @@ Update each individual cell that corresponds with a given professor with the fou
 */
 function changeRMPCells(professorPageURL, rating, professorName){
 
+    $('tbody').find('tr').each(function() {
 
-	$("tbody").find('tr').each(function(){
+        //compare in "last(+middle)+first" form.
+        var professorCell = $(this).find('td').eq(4).html();
+        // console.log(professorCell);
+        // console.log(professorName);
 
-		//compare in "last(+middle)+first" form.
-		var professorCell = $(this).find('td').eq(4).html() 
-		// console.log(professorCell);
-		// console.log(professorName);
+        if (professorCell == professorName){
 
-		if (professorCell == professorName){
+            if (professorPageURL != '?') {
 
-			if (professorPageURL != "?"){
+                if (rating != '?' && typeof rating != 'undefined') {
 
-				if (rating != "?" && typeof rating != 'undefined'){
-
-					$(this).find('td').eq(4).after(
-						'<td><pre>Overall: '+rating.overall +
-						'\nHelfulness: '+rating.helpfulness +
-						'\nClarity: '+rating.clarity +
-						'\nEasiness: '+rating.easiness +
-						' \n<a href="' + professorPageURL + '" target="_blank">More info</a></pre></td>');
-				}
-				else{
-					$(this).find('td').eq(4).after(
-						'<td><pre><a href="' + professorPageURL + '" target="_blank">Be the\nfirst to rate!</a><pre></td>');
-				}
-			}
-
-			else{
-				$(this).find('td').eq(4).after('<td><pre>No page\nwas found.<pre></td>');
-			}
-		}
-	})
+                    $(this).find('td').eq(4).after(
+                        '<td><pre>Overall: '+ rating.overall +
+                        '\nHelfulness: '+ rating.helpfulness +
+                        '\nClarity: '+ rating.clarity +
+                        '\nEasiness: '+ rating.easiness +
+                        ' \n<a href="' + professorPageURL + '" target="_blank">More info</a></pre></td>');
+                } else {
+                    $(this).find('td').eq(4).after(
+                        '<td><pre><a href="' + professorPageURL + '" target="_blank">Be the\nfirst to rate!</a><pre></td>'
+                    );
+                }
+            } else {
+                $(this).find('td').eq(4).after('<td><pre>No page\nwas found.<pre></td>');
+            }
+        }
+    });
 }
